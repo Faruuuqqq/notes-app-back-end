20397b1cd7d935b0b0eea8c9145a97329b4b24be
Faruuuqqq <faruqmahdison@gmail.com>
2024-12-19 10:39:30 +0700

    fixing bug host value

--- src/server.js
+++ src/server.js
@@ -2,15 +2,15 @@ const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 
 const init = async () => {
-  const server = Hapi.server({
+  const server = Hapi.server({    
     port: 5000,
-    host: process.env.HOST || 'localhost', // Gunakan host dari environment atau localhost
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
-      cors: {
-        origin: ['*'], // Izinkan semua origin
-      },
+        cors: {
+            origin: ['*'],
+        },
     },
-  });
+});
 
   server.route(routes);
 
